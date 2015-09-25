52dfcb7ed66a82f96312fe908cf8b7b19cdc8f7c
creightonja <creightonja@gmail.com>
2015-09-25 12:47:58 -0700

    add factory

---
+++ services/StudentsFactory.js
@@ -0,0 +1,10 @@
+studentLogin.factory('StudentsFactory', function StudentsFactory() {
+  var factory = {};
+  factory.students = [];
+  factory.addStudent = function() {
+    factory.students.push({ name: factory.studentName, id: factory.students.length + 1, login: false });
+    factory.studentName = null;
+  };
+
+  return factory;
+});
